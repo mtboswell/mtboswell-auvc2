399845f56363509c794aa2be3e9f3733f8ee0829
auvt <auvt@AUVT-SERVER>
2010-02-18 18:14:34 -0500

    Reverted model to C++ from accidental C. Add/Removed affected generated files.

--- brain/brain.h
+++ brain/brain.h
@@ -3,11 +3,11 @@
  *
  * Real-Time Workshop code generated for Simulink model brain.
  *
- * Model version                        : 1.197
+ * Model version                        : 1.198
  * Real-Time Workshop file version      : 7.3  (R2009a)  15-Jan-2009
- * Real-Time Workshop file generated on : Wed Feb 17 18:15:29 2010
+ * Real-Time Workshop file generated on : Thu Feb 18 18:09:04 2010
  * TLC version                          : 7.3 (Jan 16 2009)
- * C/C++ source code generated on       : Wed Feb 17 18:15:30 2010
+ * C/C++ source code generated on       : Thu Feb 18 18:09:04 2010
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: 32-bit Generic
@@ -472,13 +472,35 @@ extern ExternalOutputs_brain brain_Y;
 /* Constant parameters (auto storage) */
 extern const ConstParam_brain brain_ConstP;
 
-/* Model entry point functions */
-extern void brain_initialize(void);
-extern void brain_step(void);
-extern void brain_terminate(void);
+#ifdef __cplusplus
+
+extern "C" {
+
+#endif
+
+  /* Model entry point functions */
+  extern void brain_initialize(void);
+  extern void brain_step(void);
+  extern void brain_terminate(void);
+
+#ifdef __cplusplus
+
+}
+#endif
 
 /* Real-time Model object */
-extern RT_MODEL_brain *brain_M;
+#ifdef __cplusplus
+
+extern "C" {
+
+#endif
+
+  extern RT_MODEL_brain *brain_M;
+
+#ifdef __cplusplus
+
+}
+#endif
 
 /*-
  * The generated code includes comments that allow you to trace directly
