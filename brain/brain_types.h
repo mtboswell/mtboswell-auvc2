c5b33d6311c0e190f10e54726dd8a0ec31af2e4a
auvt <auvt@AUVT-SERVER>
2010-02-20 15:38:42 -0500

    Changed rate to 5Hz

--- brain/brain_types.h
+++ brain/brain_types.h
@@ -5,9 +5,9 @@
  *
  * Model version                        : 1.221
  * Real-Time Workshop file version      : 7.3  (R2009a)  15-Jan-2009
- * Real-Time Workshop file generated on : Sat Feb 20 11:33:21 2010
+ * Real-Time Workshop file generated on : Sat Feb 20 15:15:58 2010
  * TLC version                          : 7.3 (Jan 16 2009)
- * C/C++ source code generated on       : Sat Feb 20 11:33:21 2010
+ * C/C++ source code generated on       : Sat Feb 20 15:15:59 2010
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: 32-bit Generic
--- brain/rtwtypes.h
+++ brain/rtwtypes.h
@@ -4,7 +4,7 @@
  * Definitions required by Real-Time Workshop generated code.
  *
  * Real-Time Workshop version: 7.3
- * Generated on: 2010-02-20 11:33:24
+ * Generated on: 2010-02-20 15:16:01
  */
 
 #ifndef __RTWTYPES_H__
