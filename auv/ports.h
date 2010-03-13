a8dabab259a9bf5848c07a8aaa51532066e8fb48
auvt <auvt@AUVT-SERVER>
2010-03-13 17:49:59 -0500

    Tried to fix mechs scripting; fixed broadcast bug in sendCmd script;

---
+++ auv/ports.h
@@ -0,0 +1,10 @@
+#ifndef PORTS_H_
+#define PORTS_H_
+
+#define IMUPORT "/dev/ttyS0"
+#define POWERPORT "/dev/ttyS1"
+#define POLOLUPORT "/dev/ttyUSB1"
+#define ARDUINOPORT "/dev/ttyUSB0"
+#define CAMERAPORT "/dev/video0"
+
+#endif /*PORTS_H_*/
---
+++ brain/rtGetNaN.cpp
@@ -0,0 +1,84 @@
+/*
+ * File: rtGetNaN.cpp
+ *
+ * Real-Time Workshop code generated for Simulink model brain.
+ *
+ * Model version                        : 1.243
+ * Real-Time Workshop file version      : 7.3  (R2009a)  15-Jan-2009
+ * Real-Time Workshop file generated on : Tue Mar  2 17:24:21 2010
+ * TLC version                          : 7.3 (Jan 16 2009)
+ * C/C++ source code generated on       : Tue Mar  2 17:24:21 2010
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: AMD->K5/K6/Athlon
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+/*
+ * Abstract:
+ *      Real-Time Workshop function to intialize non-finite, NaN
+ */
+#include "rtGetNaN.h"
+#define NumBitsPerChar                 8
+
+extern "C" {
+  /* Function: rtGetNaN ==================================================
+   * Abstract:
+   *	Initialize rtNaN needed by the generated code.
+   *	NaN is initialized as non-signaling. Assumes IEEE.
+   */
+  real_T rtGetNaN(void)
+  {
+    size_t bitsPerReal = sizeof(real_T) * (NumBitsPerChar);
+    real_T nan = 0.0;
+    if (bitsPerReal == 32) {
+      nan = rtGetNaNF();
+    } else {
+      typedef struct {
+        struct {
+          uint32_T wordL;
+          uint32_T wordH;
+        } words;
+      } LittleEndianIEEEDouble;
+
+      union {
+        LittleEndianIEEEDouble bitVal;
+        real_T fltVal;
+      } tmpVal;
+
+      tmpVal.bitVal.words.wordH = 0xFFF80000;
+      tmpVal.bitVal.words.wordL = 0x00000000;
+      nan = tmpVal.fltVal;
+    }
+
+    return nan;
+  }
+
+  /* Function: rtGetNaNF ==================================================
+   * Abstract:
+   *	Initialize rtNaNF needed by the generated code.
+   *	NaN is initialized as non-signaling. Assumes IEEE.
+   */
+  real32_T rtGetNaNF(void)
+  {
+    typedef struct {
+      union {
+        real32_T wordLreal;
+        uint32_T wordLuint;
+      } wordL;
+    } IEEESingle;
+
+    IEEESingle nanF;
+    nanF.wordL.wordLuint = 0xFFC00000;
+    return nanF.wordL.wordLreal;
+  }
+}
+#undef NumBitsPerChar
+
+/* end rt_getNaN.c */
+
+/* File trailer for Real-Time Workshop generated code.
+ *
+ * [EOF]
+ */
