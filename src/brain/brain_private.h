79f3bd1c32f26cdf46b075da084b74af852ad273
sportner <sportner@vt.edu>
2010-07-17 15:18:08 -0400

    Merge and fixed brain IMU acceleration values one last time

--- src/brain/brain_private.h
+++ src/brain/brain_private.h
@@ -3,11 +3,11 @@
  *
  * Real-Time Workshop code generated for Simulink model brain.
  *
- * Model version                        : 1.715
+ * Model version                        : 1.720
  * Real-Time Workshop file version      : 7.5  (R2010a)  25-Jan-2010
- * Real-Time Workshop file generated on : Sat Jul 17 14:17:03 2010
+ * Real-Time Workshop file generated on : Sat Jul 17 15:15:04 2010
  * TLC version                          : 7.5 (Jan 19 2010)
- * C/C++ source code generated on       : Sat Jul 17 14:17:04 2010
+ * C/C++ source code generated on       : Sat Jul 17 15:15:05 2010
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: AMD->K5/K6/Athlon
