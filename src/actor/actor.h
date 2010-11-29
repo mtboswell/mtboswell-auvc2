0481cb6d0fbbd5b70ec214b7bff4c0f000ab8930
Micah Boswell <micah27@vt.edu>
2010-11-28 20:35:09 -0500

    Tried to figure out Actor API

--- src/actor/actor.h
+++ src/actor/actor.h
@@ -3,12 +3,20 @@
 
 #include "../simulinkmodule.h"
 #include "src/MotionController.h"
+#include "mechanisms.h"
 
 class Actor : public SimulinkModule 
 {
 	Q_OBJECT;
 	public:
-	Actor(QMap<QString, QString>* configIn, AUV_State* stateIn, QObject* parent = 0);
+		Actor(QMap<QString, QString>* configIn, AUV_State* stateIn, QObject* parent = 0);
+		
+		enum CMDS {MOVE, GOTO, TRACK, ALIGN, APPROACH, RETREAT, HOLD, SURFACE};
+	public slots:
+		void setCmd(QString cmd);
+
+	signals:
+		void statusChanged();
 	private slots:
 		void runStep();
 };
