fd776d55da00e8a1ae71931294080a2f41677089
Micah Boswell <micah27@vt.edu>
2010-12-03 15:24:20 -0500

    Misc design changes (see next changeset)

--- src/actor/actor.h
+++ src/actor/actor.h
@@ -4,24 +4,50 @@
 #include "../simulinkmodule.h"
 #include "src/MotionController.h"
 #include "mechanisms.h"
+#include "hal.h"
+#include "sal.h"
+#include "aal.h"
 
+/**
+ * Actor Module.
+ * Inputs:
+ * 	Command
+ *	Command Options:
+ *		motion
+ 			heading, depth, fwd, strafe
+ *		position
+ 			axis (x,y,z), distance
+ *		target
+ 			targetID$
+			direction?
+			maintainHeading?
+			align?
+			approachSpeed#
+ * Outputs: 
+ *	Thruster outputs
+ */
 class Actor : public SimulinkModule 
 {
 	Q_OBJECT;
 	public:
 		Actor(QMap<QString, QString>* configIn, AUV_State* stateIn, QObject* parent = 0);
+		bool ActorFinished;
 		enum ActorMode {OPENLOOPMOVING, CLOSEDLOOPMOVING, TRACKING, SURFACING};
-		enum ActorStatus {};
+		enum targetDir {FORWARD, DOWN};
 		
 	public slots:
 
+		void setCmd(QString cmd);
+
 		void move(heading, depth, fwd, strafe, timeout = inf);
-		void goTo(position = here);
+		void deadReckon(axis, distance);
 		void track(object, down/fwd, bool maintainHeading, align=long/short, int approachSpeed);
 		void surface();
 
 	signals:
 		void statusChanged();
+		void finished();
+
 	private slots:
 		void runStep();
 
