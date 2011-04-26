5048e40a2bdcba7a2f892bc95cf02199be2facaa
Carlo del Mundo <Fedora 14 - Laptop@localhost>
2011-04-26 19:39:36 -0400

    director: removing comments; adding minor stuff; new auv.lua file

--- src/director/lua/auv.lua
+++ src/director/lua/auv.lua
@@ -1,75 +1,90 @@
 state =
 {
-	Name = "DriveForward";
-	Command = "Target";
+	Name = "State1";
+	Command = "DeadReckon";
 	Options =
 	{
-		count = 2;
+		count = 1;
 		
-		o1_label = "Target.DesiredHeading";
-		o1_value = 333;
-		
-		o2_label = "Target.Type";
-		o2_value = "Buoy";
+		o1_label = "Orientation.Heading";
+		o1_value = 0;
 	};
 	
 	TriggerTransitions =
 	{
-		count = 2;
+		count = 1;
 		
 		t1_label = "Orientation.Heading";
-		t1_operator = "<";
+		t1_operator = ">";
 		t1_value = 23;
-		t1_to = "SomeOtherState";
-		
-		t2_label = "Target.Lost";
-		t2_operator = ">=";
-		t2_value = 55;
-		t2_to = "STOP";
-	};
-
-	TimerTransitions =
-	{
-		count = 2;
-		
-		t1_to = "SomeOtherState";
-		t1_time = 3200;
-		
-		t2_to = "STOP";
-		t2_time = 1422;
+		t1_to = "State2";
 	};
 };
 addState(state);
 
 state =
 {
-	Name = "SomeOtherState";
-	Command = "Target";
+	Name = "State2";
+	Command = "DeadReckon";
 	Options =
 	{
-		count = 2;
+		count = 1;
 		
-		o1_label = "Targed.DesiredHeading";
-		o1_value = 999;
-		
-		o2_label = "Target.Type";
-		o2_value = "Buoy";
+		o1_label = "DeadReckon.ForwardSpeed";
+		o1_value = 30;
 	};
 	
-	TriggerTransitions =
+	TimerTransitions =
 	{
-		count = 2;
+		count = 1;
 		
-		t1_label = "Orientation.Heading";
-		t1_operator = "<";
-		t1_value = 5;
-		t1_to = "DriveForward";
-		t1_timeEnable = 10000;	-- 10 seconds
+		t1_to = "State3";
+		t1_time = "10000";	-- ms
+	};
+};
+addState(state);
+
+state =
+{
+	Name = "State3";
+	Command = "DeadReckon";
+
+	Options = 
+	{
+		count = 1;
 		
-		t2_label = "Target.IDK";
-		t2_operator = "<";
-		t2_value = 3;
-		t2_to = "STOP";
+		o1_label = "Orientation.Heading";
+		o1_value = 180;
+	};	
+};
+addState(state);
+
+state = 
+{
+	Name = "State4";
+	Command = "DeadReckon";
+	
+	Options = 
+	{
+		count = 1;
+		
+		o1_label = "DeadReckon.ForwardSpeed";
+		o1_value = 30;
+	};
+};
+addState(state);
+
+state =
+{
+	Name = "State5";
+	Command = "DeadReckon";
+	
+	Options = 
+	{
+		count = 1;
+		
+		o1_label = "DeadReckon.ForwardSpeed";
+		o1_value = 0;
 	};
 };
 addState(state);
