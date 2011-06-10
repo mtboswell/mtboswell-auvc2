42e02ccd0635ef75afa71a32e1e4f9c8d3e29971
Carlo del Mundo <cdel@vt.edu>
2011-06-09 21:37:21 -0400

    vision: files that did not make last push

---
+++ src/vision/src/dspsizeof_rt.h
@@ -0,0 +1,19 @@
+/*
+ *  dspsizeof_rt.h
+ *
+ *  Copyright 2008-2010 The MathWorks, Inc.
+ *  $Revision: 1.1.8.1.10.1 $ $Date: 2010/07/23 15:38:12 $
+ */
+
+#ifndef dspsizeof_rt_h
+#define dspsizeof_rt_h
+
+#define MWDSP_SIZEOF_DOUBLE     sizeof(real64_T)
+#define MWDSP_SIZEOF_FLOAT      sizeof(real32_T)
+#define MWDSP_SIZEOF_BOOLEAN    sizeof(boolean_T)
+#define MWDSP_SIZEOF_INT8       sizeof(int8_T)
+#define MWDSP_SIZEOF_INT16      sizeof(int16_T)
+#define MWDSP_SIZEOF_INT32      sizeof(int32_T)
+#define MWDSP_SIZEOF_LONG       sizeof(ulong_T)
+
+#endif /* dspsizeof_rt_h */
