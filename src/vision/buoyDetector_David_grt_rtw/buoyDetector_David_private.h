4cfc2efb1636483953d6077d83de8bf78de5458e
AUVT-Server <AUVT-Server@localhost>
2011-06-21 17:21:35 -0400

    Working on buoy_detector.mdl in Test_Models

---
+++ src/vision/buoyDetector_David_grt_rtw/buoyDetector_David_private.h
@@ -0,0 +1,56 @@
+/*
+ * buoyDetector_David_private.h
+ *
+ * Real-Time Workshop code generation for Simulink model "buoyDetector_David.mdl".
+ *
+ * Model version              : 1.381
+ * Real-Time Workshop version : 7.6  (R2010b)  03-Aug-2010
+ * C source code generated on : Tue May 24 20:20:11 2011
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+#ifndef RTW_HEADER_buoyDetector_David_private_h_
+#define RTW_HEADER_buoyDetector_David_private_h_
+#include "rtwtypes.h"
+#define CALL_EVENT                     (-1)
+#ifndef __RTWTYPES_H__
+#error This file requires rtwtypes.h to be included
+#else
+#ifdef TMWTYPES_PREVIOUSLY_INCLUDED
+#error This file requires rtwtypes.h to be included before tmwtypes.h
+#else
+
+/* Check for inclusion of an incorrect version of rtwtypes.h */
+#ifndef RTWTYPES_ID_C08S16I32L32N32F1
+#error This code was generated with a different "rtwtypes.h" than the file included
+#endif                                 /* RTWTYPES_ID_C08S16I32L32N32F1 */
+#endif                                 /* TMWTYPES_PREVIOUSLY_INCLUDED */
+#endif                                 /* __RTWTYPES_H__ */
+
+/*
+ * UNUSED_PARAMETER(x)
+ *   Used to specify that a function parameter (argument) is required but not
+ *   accessed by the function body.
+ */
+#ifndef UNUSED_PARAMETER
+# if defined(__LCC__)
+#   define UNUSED_PARAMETER(x)                                   /* do nothing */
+# else
+
+/*
+ * This is the semi-ANSI standard way of indicating that an
+ * unused function parameter is required.
+ */
+#   define UNUSED_PARAMETER(x)         (void) (x)
+# endif
+#endif
+
+extern int32_T div_s32_floor(int32_T numerator, int32_T denominator);
+extern void buoyDetector_Davi_LowPassFilter(const real_T rtu_I[19200],
+  rtB_LowPassFilter_buoyDetector_ *localB);
+
+#endif                                 /* RTW_HEADER_buoyDetector_David_private_h_ */
