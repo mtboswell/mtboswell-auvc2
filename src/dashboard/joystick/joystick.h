2d249a59210654fbe0899d20893af45543d034ba
AUVT-Server <AUVT-Server@localhost>
2011-06-20 20:23:02 -0400

    joystick: forgot files

---
+++ src/dashboard/joystick/joystick.h
@@ -0,0 +1,31 @@
+#ifndef JOYSTICK_H
+#define JOYSTICK_H
+
+#include <QThread>
+
+/**
+ *  @author Carlo del Mundo
+ *  Joystick interface for immediate feedback for AUVT Control Systems; uses SDL library.
+ *      INSTALLING SDL:
+ *          1.) Change directory to ../$AUVT_SRC/SDL/
+ *          2.) # ./configure
+ *          3.) # make
+ *          4.) # make install
+ */
+class joystick  : public QThread
+{
+    Q_OBJECT
+public:
+    double forwardSpeed;
+    double depth;
+    double strafe;
+    double heading;
+
+    joystick();
+
+private:
+    void init();
+    void run();
+};
+
+#endif // JOYSTICK_H
