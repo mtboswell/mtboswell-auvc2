0f060683b2d6d70e6f805a7e038700c06b42f3a9
Carlo del Mundo <cdel@vt.edu>
2011-06-21 02:48:23 -0400

    vision: adding

---
+++ src/vision/src/VisionModel_data.cpp
@@ -0,0 +1,65 @@
+/*
+ * File: VisionModel_data.cpp
+ *
+ * Real-Time Workshop code generated for Simulink model VisionModel.
+ *
+ * Model version                        : 1.389
+ * Real-Time Workshop file version      : 7.6  (R2010b)  03-Aug-2010
+ * Real-Time Workshop file generated on : Tue Jun 21 02:40:37 2011
+ * TLC version                          : 7.6 (Jul 13 2010)
+ * C/C++ source code generated on       : Tue Jun 21 02:40:37 2011
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "VisionModel.h"
+#include "VisionModel_private.h"
+
+/* Block parameters (auto storage) */
+Parameters_VisionModel VisionModel_P = {
+  1.0,                                 /* Expression: Scale_Forward_R
+                                        * Referenced by: '<S4>/Gain'
+                                        */
+  1.0,                                 /* Expression: Scale_Forward_G
+                                        * Referenced by: '<S4>/Gain1'
+                                        */
+  1.0,                                 /* Expression: Scale_Forward_B
+                                        * Referenced by: '<S4>/Gain2'
+                                        */
+  0.8,                                 /* Expression: Scale_Forward_H
+                                        * Referenced by: '<S4>/Gain3'
+                                        */
+  1.0,                                 /* Expression: Scale_Forward_S
+                                        * Referenced by: '<S4>/Gain4'
+                                        */
+  2.0,                                 /* Expression: Scale_Forward_V
+                                        * Referenced by: '<S4>/Gain5'
+                                        */
+  1.0,                                 /* Expression: Scale_Down_R
+                                        * Referenced by: '<S2>/Gain6'
+                                        */
+  1.0,                                 /* Expression: Scale_Down_G
+                                        * Referenced by: '<S2>/Gain7'
+                                        */
+  1.0,                                 /* Expression: Scale_Down_B
+                                        * Referenced by: '<S2>/Gain8'
+                                        */
+  1.0,                                 /* Expression: Scale_Down_H
+                                        * Referenced by: '<S2>/Gain9'
+                                        */
+  1.0,                                 /* Expression: Scale_Down_S
+                                        * Referenced by: '<S2>/Gain10'
+                                        */
+  5.0                                  /* Expression: Scale_Down_V
+                                        * Referenced by: '<S2>/Gain11'
+                                        */
+};
+
+/*
+ * File trailer for Real-Time Workshop generated code.
+ *
+ * [EOF]
+ */
