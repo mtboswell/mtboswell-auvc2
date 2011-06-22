a058efe358c6d7e8cbe60efc667f2c33de893e08
AUVT-Server <AUVT-Server@localhost>
2011-06-22 00:58:06 -0400

    Update to Final_Models.

--- src/vision/Test_Models/VisionConstants.m
+++ src/vision/Test_Models/VisionConstants.m
@@ -1,16 +1,13 @@
-%   AUV stateflow stuff
-%   In particular these are the values set to the workspace before
-%   generating code for the overall stateflow chart
-%
+% AUV Vision Constants
+%   These values are set to the workspace before generating C++ code
 
-%Forward_Camera_Dimensions = [160 120];
-Forward_Camera_Dimensions = [640 480];
+
+%% Camera dimensions
+Forward_Camera_Dimensions = [160 120];
 Down_Camera_Dimensions = [120 160];
-%Forward_Camera_Dimensions = [752 480];
-%Down_Camera_Dimensions = [480 752];
 
-% The following configuration has been tested with a high success rate on
-% static images for the validation gate:
+
+%% Pre-filtering
 Scale_Forward_R = 1;
 Scale_Forward_G = 1;
 Scale_Forward_B = 1;
@@ -18,7 +15,6 @@ Scale_Forward_H = .8;
 Scale_Forward_S = 1;
 Scale_Forward_V = 2;
 
-% The following has not been tested:
 Scale_Down_R = 1;
 Scale_Down_G = 1;
 Scale_Down_B = 1;
@@ -27,43 +23,29 @@ Scale_Down_S = 1;
 Scale_Down_V = 5;
 
 
-Depth_Kp = 75;
-Depth_Kd = 0.0;
-Depth_Ki = 0.0;
+%% Iterative segmentation default value
+Iter_Segment_Thresh = 40;
 
-Heading_Kp = 0.65;
-Heading_Kd = 0.4;
-Heading_Ki = 0.0;
 
-Track_HueHigher = .1;
-Track_HueLower = .25;
-Track_Saturation = 0.5;
+%% Hedge constants
+Hedge_Hue = .4;
+Hedge_Saturation = 1;
+Hedge_Value = .75;
 
-% Old values:
-%Track_HueHigher = 0.75;
-%Track_HueLower = 0.4;
-%Track_Saturation = 0.75;
 
-Min_Blob_Size = 15;
-Max_Blob_Size = 19200;
-Max_Num_Blobs = 50;
-
-Track_Min_Eccentricity = 0.9;
-Track_Desired_Depth = 8;
-
-Cam_Down_CCWRotation_Angle = 0;
-
-Cam_Down_Yaw_XPos_Kp = 0.6;
-Cam_Down_Yaw_XPos_Kd = 0.0;
-Cam_Down_Yaw_XPos_Ki = 0.0;
-
-Cam_Down_Strafe_XPos_Kp = -1.0;
-Cam_Down_Strafe_XPos_Kd = 0.0;
-Cam_Down_Strafe_XPos_Ki = 0.0;
-
-Cam_Down_YPos_Kp = 0.75;
-Cam_Down_YPos_Kd = 0.0;
-Cam_Down_YPos_Ki = 0.0;
+%% Buoy constants
+% Color ranges for buoys (Lab color space). Indices correspond to colors:
+%   1 -> White
+%   2 -> Black
+%   3 -> Blue
+%   4 -> Red
+%   5 -> Orange
+%   6 -> Yellow
+%   7 -> Green
+a_range = [0, 0, 0, 25, 0, 0, -128;
+    0, 0, 0, 127, 0, 0, -25];
+b_range = [0, 0, 0, 0, 0, 0, -5;
+    0, 0, 0, 127, 0, 0, 127];
 
 Buoy1_Hue = 0.4;
 Buoy1_Saturation = 1.0;
@@ -73,25 +55,29 @@ Buoy2_Hue = 0.0;
 Buoy2_Saturation = 1.0;
 Buoy2_Value = 0.75;
 
-Iter_Segment_Thresh = 60;
-
-% Minimum/maximum limits on buoy eccentricity and extent
 Buoy_Min_Eccentricity = 0;
-Buoy_Max_Eccentricity = 0.7;
-Buoy_Min_Extent = 0.7;
+Buoy_Max_Eccentricity = 1.2;
+Buoy_Min_Extent = 0.6;
 Buoy_Max_Extent = 0.9;
 
-% ?
 Max_Num_Blobs_Buoy = 10;
-
-% Minimum number of buoys that make an image "valid"
-Buoy_Count_Min = 3;
 
 % Maximum number of buoys this software will detect in one image
 %   (artificial limit; may adjust)
 Buoy_Count_Max = 50;
 
-% Limits on torpedo target geometry
+Buoy_Strafe = 40;
+
+% Blob limits
+%Min_Blob_Size = 15;
+%Max_Blob_Size = 19200;
+%Max_Num_Blobs = 50;
+
+%Track_Min_Eccentricity = 0.9;
+%Track_Desired_Depth = 8;
+
+
+%% Torpedo constants
 %{
 Torpedo_Min_Area
 Torpedo_Max_Area
@@ -119,65 +105,23 @@ Torpedo_Min_Blob_Size
 Torpedo_Max_Blob_Size
 %}
 
-% Limits on torpedo target colors
 
-
-
-% Rotation to apply to forward camera image (physically mounted sideways)
-Cam_Forward_CCWRotation_Angle = 90;
-
-Cam_Forward_XPosition_Kp = 0.07;
-Cam_Forward_XPosition_Kd = 0.0;
-Cam_Forward_XPosition_Ki = 0.0;
-
-Cam_Forward_YPosition_Kp = 0.8;
-Cam_Forward_YPosition_Kd = 0.0;
-Cam_Forward_YPosition_Ki = 0.0;
-
-IMU_YVel_Kp = 0.25;
-IMU_YVel_Kd = 0.0;
-IMU_YVel_Ki = 0.0;
-
-IMU_YawRate_Kp = 0.25;
-IMU_YawRate_Kd = 0.0;
-IMU_YawRate_Ki = 0.0;
+%% Optical flow rotation constants
+Cam_Down_CCWRotation_Angle = 0;
 
 Heading_Forward_Velocity = 30;
 Vision_Forward_Velocity = 15;
 
-Buoy_Strafe = 40;
 
-Hedge_Hue = .4;
-Hedge_Saturation = 1;
-Hedge_Value = .75;
-
+%% Unknown
 RC_Source = 1;
 MissionPlan = [1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 0 0];
 
-%% Debugging
-Buoy_Hue_HalfRange = 10;
+Track_HueHigher = .1;
+Track_HueLower = .25;
+Track_Saturation = 0.5;
 
-Buoy1_Hue = 30;
-Buoy1_Saturation = 1.0;
-Buoy1_Value = 0.45;
-    
-Buoy2_Hue = 80;
-Buoy2_Saturation = 1.0;
-Buoy2_Value = 0.75;
-
-Buoy3_Hue = 15;
-Buoy3_Saturation = 1.0;
-Buoy3_Value = 0.45;
-
-Iter_Segment_Thresh = 60;
-
-Buoy_Min_Eccentricity = 0;
-Buoy_Max_Eccentricity = 1.2;
-Buoy_Min_Extent = 0.1;
-Buoy_Max_Extent = 0.9;
-
-Max_Num_Blobs_Buoy = 10;
-
-
-
-Min_Num_Buoys = 3;
+% Old values:
+%Track_HueHigher = 0.75;
+%Track_HueLower = 0.4;
+%Track_Saturation = 0.75;
