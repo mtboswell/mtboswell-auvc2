29258a612c495f02b91af0450283dc4aae58c408
AUVT-Server <AUVT-Server@localhost>
2011-06-27 18:23:52 -0400

    Working VisionModel compiled

--- src/vision/Test_Models/Tuned_Parameters/const_validation_gate.m
+++ src/vision/Test_Models/Tuned_Parameters/const_validation_gate.m
@@ -3,6 +3,6 @@ Scale_Forward_G = 1;
 Scale_Forward_B = 1;
 Scale_Forward_H = .8;
 Scale_Forward_S = 1;
-Scale_Forward_V = 2;
+Scale_Forward_V = 5;
 
-Iter_Segment_Thresh = 20;
+Iter_Segment_Thresh = 30;
