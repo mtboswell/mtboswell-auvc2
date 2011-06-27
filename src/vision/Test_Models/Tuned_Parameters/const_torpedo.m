d96e94805138e78b61b5a68b4d18e8250ff4ee72
AUVT-Server <AUVT-Server@localhost>
2011-06-27 17:15:44 -0400

    Update to Test_Models

--- src/vision/Test_Models/Tuned_Parameters/const_torpedo.m
+++ src/vision/Test_Models/Tuned_Parameters/const_torpedo.m
@@ -1,3 +1,5 @@
+Forward_Camera_Dimensions = [120 160];
+
 Scale_Down_R = 1;
 Scale_Down_G = 1;
 Scale_Down_B = 1;
@@ -24,7 +26,12 @@ a_range = [0, 0, 0, 25, 5, 0, -128;
 b_range = [0, 0, 0, 0, 30, 0, -5;
     0, 0, 0, 127, 34, 0, 127];
 
+%some old test values (deleteable)
 %   51.4618   -4.9515   37.1836
 %   31.7274    7.6845   32.4105
-    
-    
+
+
+extent_range = [0 0 0; 1 1 1];
+eccent_range = [0 0 0; 1 1 1];
+
+
