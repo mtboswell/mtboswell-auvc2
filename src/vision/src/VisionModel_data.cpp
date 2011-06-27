d373877b020f6fa9f06117aeac7d50a21c9e05f7
Carlo del Mundo <cdel@vt.edu>
2011-06-27 18:34:06 -0400

    vision: vision src

---
+++ src/vision/src/VisionModel_data.cpp
@@ -0,0 +1,50 @@
+/*
+ * File: VisionModel_data.cpp
+ *
+ * Real-Time Workshop code generated for Simulink model VisionModel.
+ *
+ * Model version                        : 1.421
+ * Real-Time Workshop file version      : 7.6  (R2010b)  03-Aug-2010
+ * Real-Time Workshop file generated on : Mon Jun 27 18:23:14 2011
+ * TLC version                          : 7.6 (Jul 13 2010)
+ * C/C++ source code generated on       : Mon Jun 27 18:23:14 2011
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "VisionModel.h"
+#include "VisionModel_private.h"
+
+/* Block parameters (auto storage) */
+Parameters_VisionModel VisionModel_P = {
+  1.0,                                 /* Expression: Scale_Forward_R
+                                        * Referenced by: '<S4>/Gain'
+                                        */
+  1.0,                                 /* Expression: Scale_Forward_G
+                                        * Referenced by: '<S4>/Gain1'
+                                        */
+  1.0,                                 /* Expression: Scale_Forward_B
+                                        * Referenced by: '<S4>/Gain2'
+                                        */
+  0.8,                                 /* Expression: Scale_Forward_H
+                                        * Referenced by: '<S4>/Gain3'
+                                        */
+  1.0,                                 /* Expression: Scale_Forward_S
+                                        * Referenced by: '<S4>/Gain4'
+                                        */
+  5.0,                                 /* Expression: Scale_Forward_V
+                                        * Referenced by: '<S4>/Gain5'
+                                        */
+  30.0                                 /* Expression: Iter_Segment_Thresh
+                                        * Referenced by: '<Root>/Constant2'
+                                        */
+};
+
+/*
+ * File trailer for Real-Time Workshop generated code.
+ *
+ * [EOF]
+ */
