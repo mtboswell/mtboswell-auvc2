c6087b04b78ace54642c02e3ef044b61c726ef98
Carlo del Mundo <cdel@vt.edu>
2011-06-27 18:34:06 -0400

    vision: vision src

---
+++ src/vision/src/validation_gate_types.h
@@ -0,0 +1,30 @@
+/*
+ * File: validation_gate_types.h
+ *
+ * Real-Time Workshop code generated for Simulink model validation_gate.
+ *
+ * Model version                        : 1.1253
+ * Real-Time Workshop file version      : 7.6  (R2010b)  03-Aug-2010
+ * Real-Time Workshop file generated on : Mon Jun 27 18:23:04 2011
+ * TLC version                          : 7.6 (Jul 13 2010)
+ * C/C++ source code generated on       : Mon Jun 27 18:23:05 2011
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_validation_gate_types_h_
+#define RTW_HEADER_validation_gate_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct RT_MODEL_validation_gate RT_MODEL_validation_gate;
+
+#endif                                 /* RTW_HEADER_validation_gate_types_h_ */
+
+/*
+ * File trailer for Real-Time Workshop generated code.
+ *
+ * [EOF]
+ */
