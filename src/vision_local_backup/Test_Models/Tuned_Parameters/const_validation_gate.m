b8911957d4caab3eac01e772c4cb4b3f57d69e75
AUVT-Server <AUVT-Server@localhost>
2011-06-28 18:13:58 -0400

    Updated Final_Models

--- src/vision_local_backup/Test_Models/Tuned_Parameters/const_validation_gate.m
+++ src/vision_local_backup/Test_Models/Tuned_Parameters/const_validation_gate.m
@@ -1,8 +1,8 @@
-Scale_Forward_R = 1;
+Scale_Forward_R = 0;
 Scale_Forward_G = 1;
-Scale_Forward_B = 1;
-Scale_Forward_H = .8;
+Scale_Forward_B = 0;
+Scale_Forward_H = 1;
 Scale_Forward_S = 1;
-Scale_Forward_V = 5;
+Scale_Forward_V = 8;
 
-Iter_Segment_Thresh = 30;
+Iter_Segment_Thresh = 40;
