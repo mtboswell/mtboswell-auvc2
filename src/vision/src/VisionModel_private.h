a5acd5689b5481e6b6ede162bd2f6308cda0a35b
mtvaleiras <mtvaleiras@localhost>
2011-07-12 20:33:36 -0400

    Added updateVision script. Unpackages the zip file and updates the agent.pro file. Updates to vision.cpp and vision.h to come

--- src/vision/src/VisionModel_private.h
+++ src/vision/src/VisionModel_private.h
@@ -3,11 +3,11 @@
  *
  * Real-Time Workshop code generated for Simulink model VisionModel.
  *
- * Model version                        : 1.470
+ * Model version                        : 1.1332
  * Real-Time Workshop file version      : 7.6  (R2010b)  03-Aug-2010
- * Real-Time Workshop file generated on : Tue Jun 28 18:13:43 2011
+ * Real-Time Workshop file generated on : Sun Jul 10 05:57:56 2011
  * TLC version                          : 7.6 (Jul 13 2010)
- * C/C++ source code generated on       : Tue Jun 28 18:13:43 2011
+ * C/C++ source code generated on       : Sun Jul 10 05:57:56 2011
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: 32-bit Generic
@@ -32,6 +32,25 @@
 #endif                                 /* RTWTYPES_ID_C08S16I32L32N32F1 */
 #endif                                 /* TMWTYPES_PREVIOUSLY_INCLUDED */
 #endif                                 /* __RTWTYPES_H__ */
+
+extern int32_T div_s32_floor(int32_T numerator, int32_T denominator);
+extern void VisionModel_validation_gate(rtB_validation_gate_VisionModel *localB,
+  rtP_validation_gate_VisionModel *localP);
+extern void VisionModel_LowPassFilter(const real_T rtu_I[19200],
+  rtB_LowPassFilter_VisionModel *localB);
+extern void VisionM_ReferenceColorSelection(const real_T rtu_L[4800], const
+  real_T rtu_a[4800], const real_T rtu_b[4800], real_T rtu_Dist_Thresh,
+  rtB_ReferenceColorSelection_Vis *localB);
+extern void VisionModel_LowPassFilter_f(const real_T rtu_I[19200],
+  rtB_LowPassFilter_VisionModel_h *localB);
+extern void VisionMod_TransformCoordinates3(real_T rtu_which_camera, real_T
+  rtu_x_in, real_T rtu_y_in, real_T rtu_z_in, const real_T
+  rtu_Camera_Dimensions[2], rtB_TransformCoordinates3_Visio *localB);
+extern void VisionModel_IfActionSubsystem(const real_T rtu_R_in[19200], real_T
+  rtu_which_camera, real_T rtu_x_in, real_T rtu_y_in, real_T rtu_z_in, const
+  real_T rtu_Camera_Dimensions[2], rtB_IfActionSubsystem_VisionMod *localB,
+  rtP_IfActionSubsystem_VisionMod *localP);
+
 #endif                                 /* RTW_HEADER_VisionModel_private_h_ */
 
 /*
--- src/vision/src/rt_nonfinite.h
+++ src/vision/src/rt_nonfinite.h
@@ -1,13 +1,18 @@
 /*
  * File: rt_nonfinite.h
  *
- * Real-Time Workshop code generated for Simulink model buoy_detector.
+ * Real-Time Workshop code generated for Simulink model VisionModel.
  *
- * Model version                        : 1.645
+ * Model version                        : 1.1332
  * Real-Time Workshop file version      : 7.6  (R2010b)  03-Aug-2010
- * Real-Time Workshop file generated on : Tue Jun  7 22:47:27 2011
+ * Real-Time Workshop file generated on : Sun Jul 10 05:57:56 2011
  * TLC version                          : 7.6 (Jul 13 2010)
- * C/C++ source code generated on       : Tue Jun  7 22:47:28 2011
+ * C/C++ source code generated on       : Sun Jul 10 05:57:56 2011
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
  */
 
 #ifndef RTW_HEADER_rt_nonfinite_h_
